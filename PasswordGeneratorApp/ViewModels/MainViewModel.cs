14a493efbf8c74ec54988a51ea84c90c39874d30
mihas48 <danilka.mihaylov.2018@yandex.ru>
2026-03-16 11:00:55 +0300

    Add commands for checkboxes (only is checked state)

--- PasswordGeneratorApp/ViewModels/MainViewModel.cs
+++ PasswordGeneratorApp/ViewModels/MainViewModel.cs
@@ -1,24 +1,23 @@
-﻿using System.Collections.ObjectModel;
-using System.Windows.Input;
-using CommunityToolkit.Mvvm;
-using CommunityToolkit.Mvvm.ComponentModel;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using System.Security.Cryptography;
+
 
 namespace PasswordGeneratorApp.ViewModels
 {
     public partial class MainViewModel : ObservableObject
     {
         [ObservableProperty]
-        private bool _isUpperCase = true;
+        private bool _isUpperCase = false;
 
         [ObservableProperty]
-        private bool _isLowerCase = true;
+        private bool _isLowerCase = false;
 
         [ObservableProperty]
-        private bool _isNumbers = true;
+        private bool _isNumbers = false;
 
         [ObservableProperty]
-        private bool _isSymbols = true;
+        private bool _isSymbols = false;
 
         [ObservableProperty]
         private int _passwordLength = 8;
@@ -47,9 +46,57 @@ namespace PasswordGeneratorApp.ViewModels
         }
 
         [RelayCommand]
+        private void ToCheckToggleButton(object parameter)
+        {
+            if (parameter is string value)
+            {
+                switch (value)
+                {
+                    case "Верхний регистр":
+                        IsUpperCase = true;
+                        break;
+                    case "Нижний регистр":
+                        IsLowerCase = true;
+                        break;
+                    case "Цифры":
+                        IsNumbers = true;
+                        break;
+                    case "Символы":
+                        IsSymbols = true;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        [RelayCommand]
         private void Generate()
         {
+            string chars = "";
 
+            if (IsUpperCase)
+                chars += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+            if (IsLowerCase)
+                chars += "abcdefghijklmnopqrstuvwxyz";
+
+            if (IsNumbers)
+                chars += "0123456789";
+
+            if (IsSymbols)
+                chars += "!@#$%^&*()_-+=";
+
+            char[] result = new char[PasswordLength];
+            byte[] randomBytes = new byte[PasswordLength];
+            RandomNumberGenerator.Fill(randomBytes);
+
+            for (int i = 0; i < PasswordLength; i++)
+            {
+                result[i] = chars[randomBytes[i] % chars.Length];
+            }
+
+            GeneratedPassword = new string(result);
         }
 
         [RelayCommand]
